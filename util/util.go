b8b893a4ee2e96aa6d3ac9e8b379a831688d94a9
macdfree <macdfree@163.com>
2022-05-24 01:22:22 +0800

    解决文件路径中有空格的问题

--- util/util.go
+++ util/util.go
@@ -8,7 +8,7 @@ import (
 
 func OpenLocal(path string) {
 	if runtime.GOOS == "windows" {
-		cmd := exec.Command(`cmd`, `/c`, `start`, path)
+		cmd := exec.Command(`cmd`, `/c`, `start`, "", path)
 		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 		cmd.Start()
 	}
